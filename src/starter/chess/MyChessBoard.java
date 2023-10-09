0c7e745bb2023408dba745fb1ac0ad9e54cf66cc
Camil Vivas <camilv12@gmail.com>
2023-10-08 19:09:51 -0600

    Implementing interfaces

---
+++ src/starter/chess/MyChessBoard.java
@@ -0,0 +1,19 @@
+package chess;
+
+public class MyChessBoard implements ChessBoard{
+
+    @Override
+    public void addPiece(ChessPosition position, ChessPiece piece) {
+
+    }
+
+    @Override
+    public ChessPiece getPiece(ChessPosition position) {
+        return null;
+    }
+
+    @Override
+    public void resetBoard() {
+
+    }
+}
---
+++ src/starter/chess/MyChessPosition.java
@@ -0,0 +1,13 @@
+package chess;
+
+public class MyChessPosition implements ChessPosition {
+    @Override
+    public int getRow() {
+        return 0;
+    }
+
+    @Override
+    public int getColumn() {
+        return 0;
+    }
+}
