e2f89abbc4785c7a5c60ac3058d1ab8be85c2531
Camil Vivas <camilv12@gmail.com>
2024-01-25 07:11:59 -0700

    Generated Equals and HashCode for Board.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,4 +1,7 @@
 package chess;
+
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -83,5 +86,18 @@ public class ChessBoard {
                 removePiece(new ChessPosition(row,col));
             }
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.equals(squares, that.squares);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(squares);
     }
 }
