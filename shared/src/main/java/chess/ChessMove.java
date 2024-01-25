5bf308e55fb06b94cd60bc81e0c097ebc48ea020
Camil Vivas <camilv12@gmail.com>
2024-01-25 07:06:12 -0700

    Generated Equals and HashCode for Move, Piece, and Position.

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -45,5 +47,18 @@ public class ChessMove {
     @Override
     public String toString(){
         return getStartPosition().toString() + " -> " + getEndPosition().toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(start, chessMove.start) && Objects.equals(end, chessMove.end) && promotion == chessMove.promotion;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end, promotion);
     }
 }
