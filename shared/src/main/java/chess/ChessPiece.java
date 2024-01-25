692ef8c9e9af2df41c04fb0ca17ee221e82a4085
Camil Vivas <camilv12@gmail.com>
2024-01-25 03:54:34 -0700

    Implemented bishop moves and toString for positions and moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -10,7 +11,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -47,6 +53,69 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> movesList = new ArrayList<>();
+        switch (type) {
+            case KING:
+                // Implement King's moves
+                break;
+            case QUEEN:
+                // Implement Queen's moves
+                break;
+            case BISHOP:
+                // Implement Bishop's moves
+                movesList = bishopMoves(board,myPosition);
+                break;
+            case KNIGHT:
+                // Implement Knight's moves
+                break;
+            case ROOK:
+                // Implement Rook's moves
+                break;
+            case PAWN:
+                // Implement Pawn's moves
+                break;
+        }
+        for(ChessMove moves : movesList){
+            System.out.println(moves.toString());
+        }
+        return movesList;
     }
+
+    private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition){
+        Collection<ChessMove> moves = new ArrayList<>();
+        addMovesInDirection(moves,board,myPosition,1,1); //Down-Right
+        addMovesInDirection(moves,board,myPosition,1,-1); //Down-Left
+        addMovesInDirection(moves,board,myPosition,-1,1); //Up-Right
+        addMovesInDirection(moves,board,myPosition,-1,-1); //Up-Left
+        return moves;
+    }
+
+    private void addMovesInDirection(Collection<ChessMove> moves, ChessBoard board, ChessPosition position, int rowIncrement, int colIncrement) {
+        //Increments the rows and columns by a certain number: 1, 0, or -1
+        int row = position.getRow() + rowIncrement;
+        int col = position.getColumn() + colIncrement;
+        while (row >= 1 && row <= 8 && col >= 1 && col <= 8) { //Iterates through the whole board,
+            ChessPosition nextPosition = new ChessPosition(row, col);
+            if (board.isOccupied(nextPosition)) { //Checks piece in the next position to check if it is an opponent. Allows opponent capture.
+                ChessPiece pieceAtNextPosition = board.getPiece(nextPosition);
+                if (pieceAtNextPosition.getTeamColor() != this.getTeamColor()) {
+                    moves.add(new ChessMove(position, nextPosition, null));
+                }
+                break; // Stop if a piece is captured or blocked
+            }
+            else {
+                moves.add(new ChessMove(position, nextPosition, null));
+            }
+            row += rowIncrement;
+            col += colIncrement;
+        }
+    }
+
+
+
+
+
+
+
+
 }
