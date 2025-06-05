18ba1ed2a06a46e2e6361f068b6b4cb619809ccc
camilv12 <camilv12@gmail.com>
2025-06-05 09:36:59 -0600

    chore: Minor refactors to ensure code quality and run server

--- client/src/test/java/client/ServerFacadeTestUtils.java
+++ client/src/test/java/client/ServerFacadeTestUtils.java
@@ -6,16 +6,16 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ServerFacadeTestUtils {
-    private static String SERVER_URL;
+    private static String serverUrl;
 
     public static void setTestUrl(int port){
-        SERVER_URL = "http://localhost:" + port;
+        serverUrl = "http://localhost:" + port;
     }
 
     // Test-only method to clear the server
     public static void clear() throws Exception {
         try(HttpClient client = HttpClient.newHttpClient()) {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(SERVER_URL + "/db")).DELETE().build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(serverUrl + "/db")).DELETE().build();
             client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (Exception e){
             throw new Exception(e.getMessage());
