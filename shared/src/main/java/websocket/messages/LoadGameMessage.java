05c6918f32f0e99f8ddf0b625ed035800d30bc70
camilv12 <camilv12@gmail.com>
2025-06-16 04:11:25 -0600

    feat(shared): Implement ServerMessage classes

--- shared/src/main/java/websocket/messages/LoadGameMessage.java
+++ shared/src/main/java/websocket/messages/LoadGameMessage.java
@@ -1,7 +1,16 @@
 package websocket.messages;
 
-public class LoadGameMessage extends ServerMessage{
-    public LoadGameMessage (int id){
+import chess.ChessGame;
 
+public class LoadGameMessage extends ServerMessage {
+    private final ChessGame game;
+
+    public LoadGameMessage(ChessGame game) {
+        super(ServerMessageType.LOAD_GAME);
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        return game;
     }
 }
